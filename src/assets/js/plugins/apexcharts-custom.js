905430d9cbfd8652adf702c80c2f0dfafbfb446b
sakibb7 <sakibb076@gmail.com>
2024-06-12 16:06:33 +0600

    updated

--- src/assets/js/plugins/apexcharts-custom.js
+++ src/assets/js/plugins/apexcharts-custom.js
@@ -1,32 +1,37 @@
+let isDark = localStorage.getItem("mode") === "dark";
+console.log(isDark);
 if (document.querySelector(".player-profile-chart")) {
   var options = {
     series: [
       {
         name: "Series 1",
-        data: [82, 83, 100, 80, 60, 80],
+        data: [80, 50, 30, 40, 100, 20],
       },
     ],
     chart: {
-      height: 350,
+      height: 310,
       type: "radar",
-      toolbar: false,
-    },
-    dataLabels: {
-      enabled: true,
-    },
-    colors: ["#FF4560"],
-    plotOptions: {
-      radar: {
-        polygons: {
-          strokeColors: "#e9e9e9",
-          fill: {
-            colors: ["#f8f8f8", "#fff"],
-          },
-        },
+      toolbar: {
+        show: false,
+      },
+      dropShadow: {
+        enabled: true,
+        blur: 1,
+        left: 1,
+        top: 1,
       },
     },
-
-    yaxis: {
+    stroke: {
+      width: 2,
+    },
+    colors: ["#FF4560"],
+    fill: {
+      opacity: 0.1,
+    },
+    markers: {
+      size: 0,
+    },
+    legend: {
       show: false,
     },
     xaxis: {
@@ -39,6 +44,12 @@ if (document.querySelector(".player-profile-chart")) {
         "Corner",
       ],
     },
+    yaxis: {
+      show: false,
+    },
+    // tooltip: {
+    //   theme: isDark ? "dark" : "light",
+    // },
   };
 
   chart = new ApexCharts(
